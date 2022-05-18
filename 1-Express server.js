f24b88b66c59c3ce16504bd37f5fa830a8544845
Yinan Li <gabriel.li5566@gmail.com>
2022-05-18 18:49:31 +0300

    express basic

---
+++ 1-Express server.js
@@ -0,0 +1,39 @@
+// 1. import express
+const express = require('express')
+// 2. create web server
+const app = express()
+
+// 3. listen the GET request from client
+// get(url, callback function(request object, response object))
+app.get('/user', (req, res) => {
+  // send the content(JSON object) to client
+  res.send({ name: 'Tom', age: 30, gender: 'male' })
+})
+
+// 4. listen the POST request from client
+// post(url, callback function(request object, response object))
+app.post('/user', (req, res) => {
+  res.send(`POST request succeeds!!`)
+})
+
+app.get('/', (req, res) => {
+  // via req.query can get the query parameter from client GET request
+  // default req.query is empty object
+  console.log(req.query)
+  res.send(req.query) // { name: 'jack', age: '20' }
+  // http://127.0.0.1/?name=jack&age=20
+})
+
+// :id is dynamic parameter (id can be changed)
+app.get('/user/:id/:name', (req, res) => {
+  // via req.params can get the dynamic parameter from client GET request
+  // default req.params is empty object
+  console.log(req.params)
+  res.send(req.params) // { id: '1' }
+  // http://127.0.0.1/user/1/rose
+})
+
+// 5. turn on the web server
+app.listen(80, () => {
+  console.log(`Express Server running at http://127.0.0.1`)
+})
---
+++ 2-express.static.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+
+// express.static(folder name) - Serving static files
+//app.use(express.static('./clock'))
+
+// Serving multiple static folders
+app.use(express.static('./files'))
+
+// if both folders have index.html, the first one will be fund and send to client
+
+// add visit prefix
+app.use('/public', express.static('./clock'))
+//http://127.0.0.1/public/index.html
+
+app.listen(80, () => {
+  console.log(`Express Server running at http://127.0.0.1`)
+})
